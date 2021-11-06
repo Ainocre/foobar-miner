4cc82741e4796d193dbb3cfee0905746821d3ad4
Guillaume Bouillé <56605479+Ainocre@users.noreply.github.com>
2021-11-06 15:27:06 +0100

    Update customHooks.test.ts

--- foobar-miner-client/src/utils/customHooks.test.ts
+++ foobar-miner-client/src/utils/customHooks.test.ts
@@ -1,8 +1,6 @@
 import { renderHook, act } from '@testing-library/react-hooks'
 import { useRobot } from './customHooks'
 
-jest.setTimeout(15000)
-
 test('should not call onCollect', () => {
   const onCollect = jest.fn()
   const { result } = renderHook(() => useRobot({ onCollect }))
